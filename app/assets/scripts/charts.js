d495f9a0bc892a360a58477644c1e5115f22e2ec
Kevin Lee <livekeen-w@kevinswkmacbook.dayforce.com>
2016-09-23 18:20:32 -0400

    Testing themes

--- app/assets/scripts/charts.js
+++ app/assets/scripts/charts.js
@@ -1,8 +1,8 @@
-define(["dojox/charting/SimpleTheme", "dojo/_base/Color", "dojox/charting/themes/common"], function(SimpleTheme, Color, themes){
+define(["dojox/charting/Theme", "dojo/_base/Color", "dojox/charting/themes/common"], function(Theme, Color, themes){
 	/*
 		A charting theme for Dayforce
 	*/
-	themes.FeussCharts = new SimpleTheme({
+	themes.FeussCharts = new Theme({
 		// The chart container
 		chart: {
 			stroke: null,
@@ -15,8 +15,14 @@ define(["dojox/charting/SimpleTheme", "dojo/_base/Color", "dojox/charting/themes
 		},
 		axis: {
 			stroke: {width: 1, color: "#adb7bc"},
+			tick: {	// used as a foundation for all ticks
+				color:     "#adb7bc",
+				position:  "center",
+				font:      "normal normal normal 7pt 'Open Sans', Helvetica, Arial, sans-serif",	// labels on axis
+				fontColor: "#292f32"	// color of labels
+			},
 			majorTick:{
-				color:	"#515e63",
+				color:	"#adb7bc",
 				width:	1,
 				length: 5
 			},
@@ -25,19 +31,19 @@ define(["dojox/charting/SimpleTheme", "dojo/_base/Color", "dojox/charting/themes
 				width:	1,
 				length:	2
 			},
-			font: "normal normal normal 8pt 'Open Sans'",
+			font: "normal normal normal 8pt 'Open Sans', Helvetica, Arial, sans-serif",
 			fontColor: "#adb7bc"
 		},
 		series: {
 			outline: null,
 			stroke: {width: 1, color: "white"},
-			font: "normal normal normal 7pt 'Open Sans'",
+			font: "normal normal normal 7pt 'Open Sans', Helvetica, Arial, sans-serif",
 			fontColor: "#292f32"
 		},
 		marker: {
 			stroke: {width: 1, color: "black"},
 			fill: "#292f32",
-			font: "normal normal normal 7pt 'Open Sans'",
+			font: "normal normal normal 7pt 'Open Sans', Helvetica, Arial, sans-serif",
 			fontColor: "black"
 		},
 		colors:[
@@ -49,6 +55,33 @@ define(["dojox/charting/SimpleTheme", "dojo/_base/Color", "dojox/charting/themes
 			Color.fromHex("#ed7dab")
 		]
 	});
+
+	// themes.FeussCharts.next = function(elementType, mixin, doPost){
+	// 	var isLine = elementType == "line";
+	// 	if(isLine || elementType == "area"){
+	// 		// custom processing for lines: substitute colors
+	// 		var s = this.seriesThemes[this._current % this.seriesThemes.length];
+	// 		s.fill.space = "plot";
+	// 		if(isLine){
+	// 			s.stroke  = { width: 4, color: s.fill.colors[0].color};
+	// 		}
+	// 		var theme = Theme.prototype.next.apply(this, arguments);
+	// 		// cleanup
+	// 		delete s.outline;
+	// 		delete s.stroke;
+	// 		s.fill.space = "shape";
+	// 		return theme;
+	// 	}
+	// 	return Theme.prototype.next.apply(this, arguments);
+	// };
+
+	// themes.FeussCharts.post = function(theme, elementType){
+	// 	theme = Theme.prototype.post.apply(this, arguments);
+	// 	if((elementType == "slice" || elementType == "circle") && theme.series.fill && theme.series.fill.type == "radial"){
+	// 		theme.series.fill = gradutils.reverse(theme.series.fill);
+	// 	}
+	// 	return theme;
+	// };
 
 	return themes.FeussCharts;
 });
