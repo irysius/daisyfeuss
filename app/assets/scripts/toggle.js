de2584764c27f43f98d75c2490c59ff7ef3719d4
Kevin Lee <livekeen-w@kevinswkmacbook.dayforce.com>
2016-10-13 15:08:06 -0400

    Test

---
+++ app/assets/scripts/toggle.js
@@ -0,0 +1,10 @@
+function toggle() {
+	var id='myElementId';
+	var myClassName=" is-active"; //must keep a space before class name
+	var d;
+
+	if (myClassName = )
+	d=document.getElementById(id);
+	d.className=d.className.replace(myClassName,""); // first remove the class name if that already exists
+	d.className = d.className + myClassName; // adding new class name
+}
